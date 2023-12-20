c2809ec6342d5c4db54ee86138dc138926935f95
pedrogzb <pegonba10@gmail.com>
2023-12-20 20:57:26 +0100

    Utilización de la hittable_list

--- RayTracingInOneWeekend/src/main.cpp
+++ RayTracingInOneWeekend/src/main.cpp
@@ -1,18 +1,17 @@
+#include "rtweekend.h"
+
+#include "color.h"
+#include "hittable.h"
+#include "hittable_list.h"
+#include "sphere.h"
+
 #include <iostream>
 #include <fstream>
 
-#include "color.h"
-#include "ray.h"
-#include "vec3.h"
-#include "hittable.h"
-#include "sphere.h"
-
-color ray_color(const ray& r) {
-    sphere S{ point3(0, 0, -1), 0.5 };
+color ray_color(const ray& r, const hittable& world) {
     hit_record rec;
-    if (S.hit(r, 0, 1000,rec)) {
-        vec3 N = rec.normal;
-        return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
+    if (world.hit(r, 0, infinity, rec)) {
+        return 0.5 * (rec.normal + color(1, 1, 1));
     }
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5 * (unit_direction.y() + 1.0);
@@ -29,6 +28,12 @@ int main() {
     // Calculate the image height, and ensure that it's at least 1.
     int image_height = static_cast<int>(image_width / aspect_ratio);
     image_height = (image_height < 1) ? 1 : image_height;
+
+    //World
+    hittable_list world;
+
+    world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
+    world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
 
     // Camera
 
@@ -61,7 +66,7 @@ int main() {
             auto ray_direction = pixel_center - camera_center;
             ray r(camera_center, ray_direction);
 
-            color pixel_color = ray_color(r);
+            color pixel_color = ray_color(r,world);
             write_color(file, pixel_color);
         }
     }
