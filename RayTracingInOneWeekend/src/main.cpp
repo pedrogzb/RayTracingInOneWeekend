dd994fdc1b32470e93b4c0ed5af23ed85e8723a4
pedrogzb <pegonba10@gmail.com>
2023-12-20 20:13:17 +0100

    Abstracción de objeto que puede ser dado por un rayo un objeto de tipo esfera

--- RayTracingInOneWeekend/src/main.cpp
+++ RayTracingInOneWeekend/src/main.cpp
@@ -4,25 +4,14 @@
 #include "color.h"
 #include "ray.h"
 #include "vec3.h"
-
-double hit_sphere(const point3& center, double radius,const ray& r) {
-    vec3 oc = r.origin() - center;
-    auto a = r.direction().length_squared();
-    auto half_b = dot(oc, r.direction());
-    auto c = oc.length_squared() - radius * radius;
-    auto discriminant = half_b * half_b - a * c;
-    
-    if (discriminant < 0) {
-        return -1.0;
-    }else {
-        return (-half_b - sqrt(discriminant)) / a;
-    }
-}
+#include "hittable.h"
+#include "sphere.h"
 
 color ray_color(const ray& r) {
-    auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
-    if (t > 0.0) {
-        vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
+    sphere S{ point3(0, 0, -1), 0.5 };
+    hit_record rec;
+    if (S.hit(r, 0, 1000,rec)) {
+        vec3 N = rec.normal;
         return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
     }
     vec3 unit_direction = unit_vector(r.direction());
