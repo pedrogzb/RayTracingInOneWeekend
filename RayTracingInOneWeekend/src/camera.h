787f515d0c42cf646c8d9fa2389b01118f724518
pedrogzb <pegonba10@gmail.com>
2023-12-21 00:06:45 +0100

    Primera implementación de un material difuso

--- RayTracingInOneWeekend/src/camera.h
+++ RayTracingInOneWeekend/src/camera.h
@@ -15,7 +15,7 @@ class camera {
         double aspect_ratio      = 1.0;  // Ratio of image width over height
         int    image_width       = 100;  // Rendered image width in pixel count
         int    samples_per_pixel = 10;   // Count of random samples for each pixel
-
+        int    max_depth         = 10;   // Maximum number of ray bounces into scene
 
         void render(const hittable& world) {
             initialize();
@@ -30,7 +30,7 @@ class camera {
                     color pixel_color(0, 0, 0);
                     for (int sample = 0; sample < samples_per_pixel; ++sample) {
                         ray r = get_ray(i, j);
-                        pixel_color += ray_color(r, world);
+                        pixel_color += ray_color(r, max_depth, world);
                     }
                     write_color(file, pixel_color, samples_per_pixel);
                 }
@@ -91,11 +91,15 @@ class camera {
             auto py = -0.5 + random_double();
             return (px * pixel_delta_u) + (py * pixel_delta_v);
         }
-        color ray_color(const ray& r, const hittable& world) const {
+        color ray_color(const ray& r, int depth, const hittable& world) const {
+            
             hit_record rec;
+            
+            if (depth <= 0) return color(0, 0, 0);
 
             if (world.hit(r, interval(0, infinity), rec)) {
-                return 0.5 * (rec.normal + color(1, 1, 1));
+                vec3 direction = random_on_hemisphere(rec.normal);
+                return 0.5 * ray_color(ray(rec.p,direction), depth-1, world);
             }
 
             vec3 unit_direction = unit_vector(r.direction());
