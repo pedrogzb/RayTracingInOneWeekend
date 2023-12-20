21f7950279d2d3d1a14ded3d1764043515ffcf73
pedrogzb <pegonba10@gmail.com>
2023-12-20 16:22:31 +0100

    Update main.cpp

--- RayTracingInOneWeekend/src/main.cpp
+++ RayTracingInOneWeekend/src/main.cpp
@@ -17,6 +17,7 @@ int main() {
     file << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = 0; j < image_height; ++j) {
+        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
             auto r = double(i) / (image_width - 1);
             auto g = double(j) / (image_height - 1);
@@ -31,5 +32,6 @@ int main() {
     }
 
     file.close();
+    std::clog << "Done";
     return 0;
 }
