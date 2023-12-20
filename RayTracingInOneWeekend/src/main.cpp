db44f13d90c0959186ff48651cab9c81e2da4bbb
pedrogzb <pegonba10@gmail.com>
2023-12-20 17:21:33 +0100

    Update main.cpp

--- RayTracingInOneWeekend/src/main.cpp
+++ RayTracingInOneWeekend/src/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <fstream>
 
+#include "color.h"
+#include "vec3.h"
+
 
 int main() {
 
@@ -19,15 +22,8 @@ int main() {
     for (int j = 0; j < image_height; ++j) {
         std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
-            auto r = double(i) / (image_width - 1);
-            auto g = double(j) / (image_height - 1);
-            auto b = 0;
-
-            int ir = static_cast<int>(255.999 * r);
-            int ig = static_cast<int>(255.999 * g);
-            int ib = static_cast<int>(255.999 * b);
-
-            file << ir << ' ' << ig << ' ' << ib << '\n';
+            auto pixel_color = color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0);;
+            write_color(file, pixel_color);
         }
     }
 
