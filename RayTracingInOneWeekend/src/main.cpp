7d9220979b94095d39fa3e0c322c80cbe9d33efa
pedrogzb <pegonba10@gmail.com>
2023-12-20 15:57:02 +0100

    Crear un archivo .ppm que muestre la imagen renderizada

---
+++ RayTracingInOneWeekend/src/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream>
+
+
+int main() {
+
+    std::ofstream file;
+    file.open("image.ppm");
+
+    // Image
+
+    int image_width = 256;
+    int image_height = 256;
+
+    // Render
+
+    file << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    for (int j = 0; j < image_height; ++j) {
+        for (int i = 0; i < image_width; ++i) {
+            auto r = double(i) / (image_width - 1);
+            auto g = double(j) / (image_height - 1);
+            auto b = 0;
+
+            int ir = static_cast<int>(255.999 * r);
+            int ig = static_cast<int>(255.999 * g);
+            int ib = static_cast<int>(255.999 * b);
+
+            file << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+    }
+
+    file.close();
+    return 0;
+}
