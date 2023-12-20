43c4a383a2044ade7feab85cc0c796e8556099ed
pedrogzb <pegonba10@gmail.com>
2023-12-20 23:35:11 +0100

    Añadido supersampling

--- RayTracingInOneWeekend/src/camera.h
+++ RayTracingInOneWeekend/src/camera.h
@@ -12,8 +12,10 @@
 
 class camera {
     public:
-        double aspect_ratio = 1.0;  // Ratio of image width over height
-        int    image_width = 100;  // Rendered image width in pixel count
+        double aspect_ratio      = 1.0;  // Ratio of image width over height
+        int    image_width       = 100;  // Rendered image width in pixel count
+        int    samples_per_pixel = 10;   // Count of random samples for each pixel
+
 
         void render(const hittable& world) {
             initialize();
@@ -25,12 +27,12 @@ class camera {
             for (int j = 0; j < image_height; ++j) {
                 std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
                 for (int i = 0; i < image_width; ++i) {
-                    auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
-                    auto ray_direction = pixel_center - center;
-                    ray r(center, ray_direction);
-
-                    color pixel_color = ray_color(r, world);
-                    write_color(file, pixel_color);
+                    color pixel_color(0, 0, 0);
+                    for (int sample = 0; sample < samples_per_pixel; ++sample) {
+                        ray r = get_ray(i, j);
+                        pixel_color += ray_color(r, world);
+                    }
+                    write_color(file, pixel_color, samples_per_pixel);
                 }
             }
 
@@ -70,6 +72,24 @@ class camera {
             auto viewport_upper_left =
                 center - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
             pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+        }
+        ray get_ray(int i, int j) const {
+            // Get a randomly sampled camera ray for the pixel at location i,j.
+
+            auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
+            auto pixel_sample = pixel_center + pixel_sample_square();
+
+            auto ray_origin = center;
+            auto ray_direction = pixel_sample - ray_origin;
+
+            return ray(ray_origin, ray_direction);
+        }
+
+        vec3 pixel_sample_square() const {
+            // Returns a random point in the square surrounding a pixel at the origin.
+            auto px = -0.5 + random_double();
+            auto py = -0.5 + random_double();
+            return (px * pixel_delta_u) + (py * pixel_delta_v);
         }
         color ray_color(const ray& r, const hittable& world) const {
             hit_record rec;
